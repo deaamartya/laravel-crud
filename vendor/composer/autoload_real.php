c14389638fe41f103c411a652c3432bf11029ca9
deaamartya <deaamartya3@gmail.com>
2021-01-19 01:33:07 +0700

    master

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit33108efb634989a17f5ae1b67ac200d9
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
